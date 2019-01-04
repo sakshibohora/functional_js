13af933b3c8e1c0eec1616e6d6ed351ea2d21c89
sakshibohora <46265763+sakshibohora@users.noreply.github.com>
2019-01-04 16:52:41 +0530

    Add files via upload

---
+++ blocking_event_loop.js
@@ -0,0 +1,17 @@
+
+function repeat(operation, num) {
+	if (num <= 0) return
+
+	operation()
+
+	
+	if (num = 10) {
+	  setTimeout(function() {
+	    repeat(operation, --num)
+	  })
+	} else {
+	  repeat(operation, --num)
+	}
+}
+
+module.exports = repeat
